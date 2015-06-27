e6de365248669c740e8c1194f4f8f337f54aeb3e
meganii <yuhei24@gmail.com>
2015-06-27 10:05:24 +0900

    Initial Commit

---
+++ FFViewer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FFViewer
+//
+//  Created by  Yuhei Tsubokawa on 2015/06/27.
+//  Copyright (c) 2015年  Yuhei Tsubokawa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
